01d9375e13f0f3ee3705deba7d5179651ce53acc
Nícolas do Couto Chaves <100481997+nicolascoutochaves@users.noreply.github.com>
2024-12-27 13:25:02 -0300

    melhorado a randomizacao

--- trabalho1/arvore/main.c
+++ trabalho1/arvore/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <time.h>
 #include "abp.h"
-#define MAX 1000000
+#define MAX INT_MAX
 int main(){
     pNodoA* a = NULL;
     for(int i = 0; i < N_DATA; i++){
         srand(time(NULL));
         a = InsereArvore(a,rand()%MAX);
     }
-    search(a, 10);
+    srand(time(NULL));
+    search(a, rand()%MAX);
     /* int c = search(a, 10);
     if (c != -1)
         printf ("Elemento encontrado: %d\n", c);
